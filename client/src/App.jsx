b697cf934fb8eae22d436dbb644516e17fcf7edb
Harsh <harshb15003@gmail.com>
2024-03-03 14:23:45 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
+import React from "react";
+import Home from "./Pages/Home";
+import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <h1>Electrothon</h1>
-    </>
-  );
-}
+
+const App = () => (
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
