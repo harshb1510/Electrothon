f4aabc0c98916613479a2be926b92aaffc049831
Akshat Gupta <akshatgtc@gmail.com>
2024-03-04 00:54:22 +0530

    blockchain

---
+++ smart_contracts/contracts/Transactions.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Transactions {
+    event Transfer(address from, address receiver, uint amount);
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount) public {
+        transactions.push(TransferStruct(msg.sender, receiver, amount));
+
+        emit Transfer(msg.sender, receiver, amount);
+    }
+}
