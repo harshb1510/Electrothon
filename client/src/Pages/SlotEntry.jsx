f835ae530cf2c5cb994512838ff9acc8915699f3
Harsh <harshb15003@gmail.com>
2024-03-05 01:18:45 +0530

    /admin

--- client/src/Pages/SlotEntry.jsx
+++ client/src/Pages/SlotEntry.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Scanner } from '@yudiel/react-qr-scanner';
 
-const SlotReader = () => {
+const SlotEntry = () => {
     const qrData = (text) => {
         const slotBooking = JSON.parse(text);
         console.log('Slot Booking:', slotBooking.slotId);
     }
   return (
     <div className='h-[400px] w-[400px] m-auto mt-[200px]'>
-
     <Scanner
             onResult={(text, result) => qrData(text,result)}
             onError={(error) => console.log(error?.message)}
             />
-            </div>
+    </div>
   )
 }
 
-export default SlotReader
+export default SlotEntry
